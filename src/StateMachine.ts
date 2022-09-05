1eaf26291202e8f052d69d3271b087df831d3d2d
Luis De Anda <ldeandac16@gmail.com>
2022-09-05 18:58:53 -0500

    Add input and output processing

--- src/StateMachine.ts
+++ src/StateMachine.ts
@@ -1,7 +1,10 @@
+import set from 'lodash/set';
+import cloneDeep from 'lodash/cloneDeep';
 import { JSONPath as jp } from 'jsonpath-plus';
 import { AllStates } from './typings/AllStates';
 import { StateMachineDefinition } from './typings/StateMachineDefinition';
 import { StateType } from './typings/StateType';
+import { isPlainObj } from './util';
 
 type StateHandler = {
   [T in StateType]: () => Promise<void>;
@@ -10,7 +13,9 @@ type StateHandler = {
 export class StateMachine {
   private currStateName: string;
   private currState: AllStates;
+  private rawInput: Record<string, unknown>;
   private currInput: Record<string, unknown>;
+  private currResult: unknown;
   private context: Record<string, unknown>;
   private readonly states: Record<string, AllStates>;
   private readonly stateHandlers: StateHandler;
@@ -19,7 +24,9 @@ export class StateMachine {
     this.states = definition.States;
     this.currStateName = definition.StartAt;
     this.currState = this.states[this.currStateName];
-    this.currInput = input;
+    this.rawInput = input;
+    this.currInput = this.rawInput;
+    this.currResult = null;
     this.context = {};
     this.stateHandlers = {
       Task: this.handleTaskState.bind(this),
@@ -38,7 +45,11 @@ export class StateMachine {
     do {
       this.currState = this.states[this.currStateName];
 
+      this.processInput();
+
       await this.stateHandlers[this.currState.Type]();
+
+      this.processResult();
 
       if ('Next' in this.currState) {
         this.currStateName = this.currState.Next;
@@ -50,15 +61,92 @@ export class StateMachine {
     } while (!isEndState);
   }
 
-  async handleTaskState() {
+  private processInputPath() {
+    if ('InputPath' in this.currState) {
+      if (this.currState.InputPath === null) {
+        return {};
+      }
+
+      return this.jsonQuery(this.currState.InputPath!, this.currInput);
+    }
+
+    return this.currInput;
+  }
+
+  private processPayloadTemplate(payloadTemplate: Record<string, unknown>, json: Record<string, unknown>) {
+    const resolvedProperties = Object.entries(payloadTemplate).map(([key, value]) => {
+      let sanitizedKey = key;
+      let resolvedValue = value;
+
+      if (isPlainObj(value)) {
+        resolvedValue = this.processPayloadTemplate(value as Record<string, unknown>, json);
+      }
+
+      if (key.endsWith('.$') && typeof value === 'string') {
+        sanitizedKey = key.replace('.$', '');
+        resolvedValue = this.jsonQuery(value, json);
+      }
+
+      return [sanitizedKey, resolvedValue];
+    });
+
+    return Object.fromEntries(resolvedProperties);
+  }
+
+  private processInput() {
+    this.currInput = this.processInputPath();
+    if ('Parameters' in this.currState) {
+      this.currInput = this.processPayloadTemplate(this.currState.Parameters!, this.currInput);
+    }
+  }
+
+  private processResultPath() {
+    if ('ResultPath' in this.currState) {
+      if (this.currState.ResultPath === null) {
+        return this.rawInput;
+      }
+
+      const sanitizedPath = this.currState.ResultPath!.replace('$.', '');
+      return set(cloneDeep(this.rawInput), sanitizedPath, this.currResult);
+    }
+
+    return this.currResult;
+  }
+
+  private processOutputPath() {
+    if ('OutputPath' in this.currState) {
+      if (this.currState.OutputPath === null) {
+        return {};
+      }
+
+      return this.jsonQuery(this.currState.OutputPath!, this.currResult as Record<string, unknown>);
+    }
+
+    return this.currResult;
+  }
+
+  private processResult() {
+    if ('ResultSelector' in this.currState) {
+      this.currResult = this.processPayloadTemplate(
+        this.currState.ResultSelector!,
+        this.currResult as Record<string, unknown>
+      );
+    }
+
+    this.currResult = this.processResultPath();
+
+    this.currResult = this.processOutputPath();
+  }
+
+  private async handleTaskState() {
     // TODO: Implement task state handler
   }
 
-  jsonQuery(pathExpression: string) {
+  private jsonQuery(pathExpression: string, json: Record<string, unknown>) {
     if (pathExpression.startsWith('$$')) {
-      return jp({ path: pathExpression.slice(1), json: this.context });
+      return jp({ path: pathExpression.slice(1), json: this.context, wrap: false });
     }
 
-    return jp({ path: pathExpression, json: this.currInput });
+    return jp({ path: pathExpression, json, wrap: false });
   }
 }
