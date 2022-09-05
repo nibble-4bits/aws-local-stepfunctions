3756efd621452858a1481cd45dc75d3892d2156a
Luis De Anda <ldeandac16@gmail.com>
2022-09-05 12:23:35 -0500

    Remove `End` property from `NextableState` definition

--- src/typings/IntermediateState.ts
+++ src/typings/IntermediateState.ts
@@ -4,7 +4,6 @@ import { StateType } from './StateType';
 interface NextableState extends BaseState {
   Type: Exclude<StateType, 'Choice' | 'Succeed' | 'Fail'>;
   Next: string;
-  End: never;
 }
 
 export type IntermediateState = NextableState;
