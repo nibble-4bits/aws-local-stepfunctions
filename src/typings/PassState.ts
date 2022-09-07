0980deed8d895067cf7aa127590ee2df912b2090
Luis De Anda <ldeandac16@gmail.com>
2022-09-07 13:16:52 -0500

    Add support for `Pass` state

--- src/typings/PassState.ts
+++ src/typings/PassState.ts
@@ -1,11 +1,12 @@
 import { BaseState } from './BaseState';
 import { CanHaveInputPath, CanHaveOutputPath, CanHaveParameters, CanHaveResultPath } from './InputOutputProcessing';
 import { IntermediateState } from './IntermediateState';
+import { JSONValue } from './JSONValue';
 import { TerminalState } from './TerminalState';
 
 interface BasePassState extends BaseState, CanHaveInputPath, CanHaveParameters, CanHaveResultPath, CanHaveOutputPath {
   Type: 'Pass';
-  Result?: unknown;
+  Result?: JSONValue;
 }
 
 export type PassState = (IntermediateState | TerminalState) & BasePassState;
