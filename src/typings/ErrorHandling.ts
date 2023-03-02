70ac1f281f8a5ea07b262c0d1f26831c616a2d95
Luis De Anda <ldeandac16@gmail.com>
2023-03-01 20:29:07 -0600

    Create types for error handling

---
+++ src/typings/ErrorHandling.ts
@@ -0,0 +1,25 @@
+type Retrier = {
+  ErrorEquals: string[];
+  IntervalSeconds?: number; // default 1
+  MaxAttempts?: number; // default 3
+  BackoffRate?: number; // default 2.0
+};
+
+export interface RetryableState {
+  Retry?: Retrier[];
+}
+
+type Catcher = {
+  ErrorEquals: string[];
+  Next: string;
+  ResultPath?: string;
+};
+
+export interface CatchableState {
+  Catch?: Catcher[];
+}
+
+export interface ErrorOutput {
+  Error: string;
+  Cause?: string;
+}
