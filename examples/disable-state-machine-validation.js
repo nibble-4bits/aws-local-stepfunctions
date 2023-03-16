75620149d9dc766c1f894ba6658bcb7fcea5d0f1
Luis De Anda <ldeandac16@gmail.com>
2023-03-15 19:10:24 -0600

    Add example for state machine definition validation

---
+++ examples/disable-state-machine-validation.js
@@ -0,0 +1,23 @@
+import { StateMachine } from 'aws-local-stepfunctions';
+
+const machineDefinition = {
+  Comment: 'A simple minimal example of the States language',
+  StartAt: 'Hello World',
+  States: {
+    'Hello World': {
+      Type: 'Task',
+      InputPath: 'invalid JSONPath syntax',
+      Resource: 'invalid arn syntax',
+      End: true,
+    },
+  },
+};
+
+// Construct a new state machine with the given definition and don't validate ARNs nor JSONPaths
+const stateMachine = new StateMachine(machineDefinition, {
+  checkArn: false,
+  checkPaths: false,
+});
+
+// The following log is printed because no error was thrown by the constructor
+console.log('No error was thrown!');
