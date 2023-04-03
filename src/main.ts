8ee3caa60733c3c7eede269f4a6f4b5f00e77a87
Luis De Anda <ldeandac16@gmail.com>
2023-04-02 21:38:36 -0600

    Export `ExecutionError` so consumers can catch it and act accordingly

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 export { StateMachine } from './stateMachine/StateMachine';
+export { ExecutionError } from './error/ExecutionError';
 export { ExecutionAbortedError } from './error/ExecutionAbortedError';
