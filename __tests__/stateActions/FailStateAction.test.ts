f31f52aad8b26839db11387e756d9faa09ce4dc3
Luis De Anda <ldeandac16@gmail.com>
2023-04-07 15:30:18 -0600

    Update `FailStateAction` test cases

--- __tests__/stateActions/FailStateAction.test.ts
+++ __tests__/stateActions/FailStateAction.test.ts
@@ -1,12 +1,13 @@
 import type { FailState } from '../../src/typings/FailState';
 import { FailStateAction } from '../../src/stateMachine/stateActions/FailStateAction';
+import { FailStateError } from '../../src/error/FailStateError';
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe('Fail State', () => {
-  test('should return true for end state flag', async () => {
+  test('should throw `FailStateError`', async () => {
     const definition: FailState = {
       Type: 'Fail',
     };
@@ -17,8 +18,28 @@ describe('Fail State', () => {
     const context = {};
 
     const failStateAction = new FailStateAction(definition);
-    const { isEndState } = await failStateAction.execute(input, context);
+    const failStateResult = failStateAction.execute(input, context);
 
-    expect(isEndState).toBe(true);
+    await expect(failStateResult).rejects.toThrow(FailStateError);
+    await expect(failStateResult).rejects.toThrow('Execution failed because of a Fail state');
+  });
+
+  test('should throw `FailStateError` with `Cause` value as error message', async () => {
+    const definition: FailState = {
+      Type: 'Fail',
+      Error: 'Failure',
+      Cause: 'This is the cause of the error',
+    };
+    const input = {
+      prop1: 'test',
+      prop2: 12345,
+    };
+    const context = {};
+
+    const failStateAction = new FailStateAction(definition);
+    const failStateResult = failStateAction.execute(input, context);
+
+    await expect(failStateResult).rejects.toThrow(FailStateError);
+    await expect(failStateResult).rejects.toThrow('This is the cause of the error');
   });
 });
