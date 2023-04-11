dfaad7d7d959508588c6521678262b8d700c641f
Luis De Anda <ldeandac16@gmail.com>
2023-04-10 22:20:13 -0600

    Create base typings for Parallel state

--- src/typings/AllStates.ts
+++ src/typings/AllStates.ts
@@ -1,9 +1,18 @@
 import type { ChoiceState } from './ChoiceState';
 import type { FailState } from './FailState';
 import type { MapState } from './MapState';
+import type { ParallelState } from './ParallelState';
 import type { PassState } from './PassState';
 import type { SucceedState } from './SucceedState';
 import type { TaskState } from './TaskState';
 import type { WaitState } from './WaitState';
 
-export type AllStates = TaskState | MapState | PassState | WaitState | ChoiceState | SucceedState | FailState;
+export type AllStates =
+  | TaskState
+  | ParallelState
+  | MapState
+  | PassState
+  | WaitState
+  | ChoiceState
+  | SucceedState
+  | FailState;
