8ea9278fc4351ff2aef8d5d7b02271aec3e1a376
Luis De Anda <ldeandac16@gmail.com>
2023-04-30 00:33:08 -0600

    Create handler class for States.Format function

---
+++ src/stateMachine/intrinsicFunctions/StatesFormat.ts
@@ -0,0 +1,36 @@
+import type { IntrinsicFunctionDefinition } from '../../typings/IntrinsicFunctionsImplementation';
+import type { JSONValue, JSONPrimitiveValue } from '../../typings/JSONValue';
+import { BaseIntrinsicFunction } from './BaseIntrinsicFunction';
+
+class StatesFormat extends BaseIntrinsicFunction {
+  protected readonly funcDefinition: IntrinsicFunctionDefinition;
+
+  constructor() {
+    super();
+
+    this.funcDefinition = {
+      name: 'States.Format',
+      minArgs: 1,
+      arguments: [
+        {
+          allowedTypes: ['string'],
+        },
+      ],
+      variadicArguments: {
+        allowedTypes: ['string', 'boolean', 'number', 'null'],
+      },
+    };
+  }
+
+  protected execute(templateStr: string, ...placeholderValues: JSONPrimitiveValue[]): JSONValue {
+    const placeholdersNumber = templateStr.match(/\{\}/g)?.length ?? 0;
+    if (placeholdersNumber !== placeholderValues.length) {
+      throw new Error(`Number of arguments in ${this.funcDefinition.name} do not match the occurrences of {}`);
+    }
+
+    let i = 0;
+    return templateStr.replace(/\{\}/g, () => placeholderValues[i++] as string);
+  }
+}
+
+export { StatesFormat };
