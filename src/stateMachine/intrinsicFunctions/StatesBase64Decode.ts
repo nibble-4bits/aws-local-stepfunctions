c65377f81c244ef7f4c7d461f0dc0011eba82c8b
Luis De Anda <ldeandac16@gmail.com>
2023-04-30 10:30:55 -0600

    Create handler class for States.Base64Decode function

---
+++ src/stateMachine/intrinsicFunctions/StatesBase64Decode.ts
@@ -0,0 +1,33 @@
+import type { IntrinsicFunctionDefinition } from '../../typings/IntrinsicFunctionsImplementation';
+import type { JSONValue } from '../../typings/JSONValue';
+import { BaseIntrinsicFunction } from './BaseIntrinsicFunction';
+
+class StatesBase64Decode extends BaseIntrinsicFunction {
+  protected readonly funcDefinition: IntrinsicFunctionDefinition;
+
+  constructor() {
+    super();
+
+    this.funcDefinition = {
+      name: 'States.Base64Decode',
+      exactArgs: 1,
+      arguments: [
+        {
+          allowedTypes: ['string'],
+        },
+      ],
+    };
+  }
+
+  protected execute(str: string): JSONValue {
+    if (str.length > 10000) {
+      throw new Error(
+        `Intrinsic function ${this.funcDefinition.name} cannot decode a Base64 string with more than 10,000 characters`
+      );
+    }
+
+    return atob(str);
+  }
+}
+
+export { StatesBase64Decode };
