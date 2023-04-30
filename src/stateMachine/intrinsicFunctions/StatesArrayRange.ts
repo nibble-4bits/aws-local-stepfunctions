df73e0e004cd4e83cd194f557370ed015eb5f11b
Luis De Anda <ldeandac16@gmail.com>
2023-04-30 12:08:03 -0600

    Create handler class for States.ArrayRange function

---
+++ src/stateMachine/intrinsicFunctions/StatesArrayRange.ts
@@ -0,0 +1,48 @@
+import type { IntrinsicFunctionDefinition } from '../../typings/IntrinsicFunctionsImplementation';
+import type { JSONValue } from '../../typings/JSONValue';
+import { BaseIntrinsicFunction } from './BaseIntrinsicFunction';
+
+class StatesArrayRange extends BaseIntrinsicFunction {
+  protected readonly funcDefinition: IntrinsicFunctionDefinition;
+
+  constructor() {
+    super();
+
+    this.funcDefinition = {
+      name: 'States.ArrayRange',
+      exactArgs: 3,
+      arguments: [
+        {
+          allowedTypes: ['number'],
+          constraints: ['INTEGER'],
+        },
+        {
+          allowedTypes: ['number'],
+          constraints: ['INTEGER'],
+        },
+        {
+          allowedTypes: ['number'],
+          constraints: ['POSITIVE_INTEGER', 'NEGATIVE_INTEGER'],
+        },
+      ],
+    };
+  }
+
+  protected execute(start: number, end: number, step: number): JSONValue {
+    const slots = (end - start) / step;
+    if (slots < 0) {
+      return [];
+    }
+
+    const arrLength = Math.floor(slots) + 1;
+    if (arrLength > 1000) {
+      throw new Error(`Result of intrinsic function ${this.funcDefinition.name} cannot contain more than 1000 items`);
+    }
+
+    const range = Array.from({ length: arrLength }).map((_, i) => start + step * i);
+
+    return range;
+  }
+}
+
+export { StatesArrayRange };
