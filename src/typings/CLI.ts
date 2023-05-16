1bc4398e45e0892f2a72c8fd4c17d5911a77ff94
Luis De Anda <ldeandac16@gmail.com>
2023-05-16 13:46:11 -0600

    Create enum for CLI exit codes

--- src/typings/CLI.ts
+++ src/typings/CLI.ts
@@ -9,3 +9,15 @@ export type ParsedCommandOptions = {
   jsonpathValidation: boolean;
   arnValidation: boolean;
 };
+
+export enum ExitCodes {
+  Success,
+  /**
+   * Represents the failure of the command before it could run an execution, e.g. if argument parsing failed.
+   */
+  PreExecutionFailure,
+  /**
+   * Represents the failure of the command because any of the executions failed.
+   */
+  StateMachineExecutionFailure,
+}
