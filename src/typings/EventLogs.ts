e065aeb449c55e976f231bd4f93eab7b77840700
Luis De Anda <ldeandac16@gmail.com>
2023-08-31 18:30:01 -0600

    Create typings for event logs

---
+++ src/typings/EventLogs.ts
@@ -0,0 +1,44 @@
+import type { ErrorOutput } from './ErrorHandling';
+import type { JSONValue } from './JSONValue';
+import type { StateType } from './StateType';
+
+type ExecutionEventType =
+  | 'ExecutionStarted'
+  | 'ExecutionSucceeded'
+  | 'MapIterationStarted'
+  | 'MapIterationSucceeded'
+  | 'ParallelBranchStarted'
+  | 'ParallelBranchSucceeded'
+  | 'ExecutionAborted'
+  | 'ExecutionTimedOut';
+type ExecutionFailedEventType = 'ExecutionFailed' | 'ParallelBranchFailed' | 'MapIterationFailed';
+type StateEventType = 'StateEntered' | 'StateExited';
+
+type AllEventTypes = ExecutionEventType | ExecutionFailedEventType | StateEventType;
+
+interface BaseEvent {
+  type: AllEventTypes;
+  timestamp: number;
+}
+
+interface ExecutionEvent extends BaseEvent {
+  type: ExecutionEventType;
+}
+
+interface ExecutionFailedEvent extends BaseEvent, ErrorOutput {
+  type: ExecutionFailedEventType;
+}
+
+interface StateData {
+  name: string;
+  type: StateType;
+  input: JSONValue;
+  output?: JSONValue;
+}
+
+interface StateEvent extends BaseEvent {
+  type: StateEventType;
+  state: StateData;
+}
+
+export type EventLog = ExecutionEvent | ExecutionFailedEvent | StateEvent;
