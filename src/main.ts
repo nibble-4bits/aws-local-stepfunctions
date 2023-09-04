f0d4ad8d3442461be2b83fb8bc4ec6e6413c31fc
Luis De Anda <ldeandac16@gmail.com>
2023-09-04 16:18:20 -0600

    Export types from API methods parameters

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+export type { StateMachineDefinition } from './typings/StateMachineDefinition';
+export type { StateMachineOptions, RunOptions } from './typings/StateMachineImplementation';
 export { StateMachine } from './stateMachine/StateMachine';
 export { ExecutionError } from './error/ExecutionError';
 export { ExecutionAbortedError } from './error/ExecutionAbortedError';
