d27ed3b9410fa2b3eff2d1134845f50731ab7eae
Luis De Anda <ldeandac16@gmail.com>
2023-09-14 19:49:56 -0600

    Create constraint to verify if a JSONPath points to a defined value

---
+++ src/stateMachine/jsonPath/constraints/DefinedValueConstraint.ts
@@ -0,0 +1,10 @@
+import { BaseJSONPathConstraint } from './BaseJsonPathConstraint';
+import { StatesRuntimeError } from '../../../error/predefined/StatesRuntimeError';
+
+export class DefinedValueConstraint extends BaseJSONPathConstraint {
+  test(value: unknown): void {
+    if (typeof value === 'undefined') {
+      throw new StatesRuntimeError(`JSONPath expression '${this.pathExpression}' does not point to a value`);
+    }
+  }
+}
