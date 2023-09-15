df56140fffac558358f63fceb7ea7638adc0d47b
Luis De Anda <ldeandac16@gmail.com>
2023-09-14 19:54:18 -0600

    Create constraint to verify if a JSONPath result is an RFC3339 timestamp

---
+++ src/stateMachine/jsonPath/constraints/RFC3339TimestampConstraint.ts
@@ -0,0 +1,13 @@
+import { BaseJSONPathConstraint } from './BaseJsonPathConstraint';
+import { isRFC3339Date } from '../../../util';
+import { StatesRuntimeError } from '../../../error/predefined/StatesRuntimeError';
+
+export class RFC3339TimestampConstraint extends BaseJSONPathConstraint {
+  test(value: unknown): void {
+    if (typeof value !== 'string' || !isRFC3339Date(value)) {
+      throw new StatesRuntimeError(
+        `JSONPath expression '${this.pathExpression}' evaluated to ${value}, but expected a timestamp conforming to the RFC3339 profile`
+      );
+    }
+  }
+}
