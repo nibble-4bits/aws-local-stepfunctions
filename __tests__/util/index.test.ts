dc64d9418a539c7f06c1b8cf4ba009249307f286
Luis De Anda <ldeandac16@gmail.com>
2023-09-15 20:07:02 -0600

    Add test for util function `isRFC3339Date`

--- __tests__/util/index.test.ts
+++ __tests__/util/index.test.ts
@@ -1,4 +1,4 @@
-import { isPlainObj, sleep } from '../../src/util';
+import { isPlainObj, sleep, isRFC3339Date } from '../../src/util';
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -56,6 +56,65 @@ describe('Utils', () => {
       rootAbortController.abort();
 
       await expect(sleepPromise).resolves.not.toThrow();
+    });
+  });
+
+  describe('isRFC3339Date', () => {
+    const positiveCases = [
+      '2023-09-15T18:25:25Z',
+      '2023-01-01T00:30:55+06:00',
+      '2023-02-05T06:12:18-06:00',
+      '2023-03-10T03:09:41+23:59',
+      '2023-04-15T23:59:59-23:59',
+      '2023-05-20T20:51:48+00:30',
+      '2023-06-25T12:07:36-00:30',
+      '2023-07-30T19:49:25.1234Z',
+      '2023-08-31T15:02:25.46195386+06:00',
+      '2023-09-24T01:23:25.5-06:00',
+      '2023-10-12T11:15:25.83+23:59',
+      '2023-11-16T09:20:25.956-23:59',
+      '2023-12-27T16:45:25.19864+00:30',
+      '2023-05-19T22:37:25.173064-00:30',
+    ];
+
+    describe.each(positiveCases)('Positive tests', (dateStr) => {
+      test(`should return true if timestamp ${dateStr} conforms to RFC3339 profile`, () => {
+        const result = isRFC3339Date(dateStr);
+
+        expect(result).toBe(true);
+      });
+    });
+
+    const negativeCases = [
+      '2023',
+      '2023-09',
+      '2023-09-15',
+      '2023-09-15T',
+      '2023-09-15T18',
+      '2023-09-15T18:25',
+      '2023-09-1518:25:25',
+      '2023-09-15T18:25:25',
+      '2023-13-01T00:30:55+06:00',
+      '2023-02-78T06:12:18-06:00',
+      '2023-03-10T24:09:41+23:59',
+      '2023-04-15T23:60:99-23:59',
+      '2023-05-20T20:51:48+35:30',
+      '2023-06-25T12:07:36-00:65',
+      '2023-07-30T19:49:25:1234Z',
+      '2023-08-31T15:02:25.46195386Z+06.00',
+      '2023-09-24T01:23:25.5-06:00Z',
+      '500-10-12T11:15:25.83+23:59',
+      '2023-11-16-09:20:25.956-23:59',
+      '2023-12-27T16:45:25.19864+00',
+      '2023-05-19T22:37:25.173064.00:30',
+    ];
+
+    describe.each(negativeCases)('Negative tests', (dateStr) => {
+      test(`should return false if timestamp ${dateStr} does not conform to RFC3339 profile`, () => {
+        const result = isRFC3339Date(dateStr);
+
+        expect(result).toBe(false);
+      });
     });
   });
 });
