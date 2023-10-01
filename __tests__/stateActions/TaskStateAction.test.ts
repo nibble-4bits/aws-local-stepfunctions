191bcfab711f0888915f5194ea092dcc2162cc87
Luis De Anda <ldeandac16@gmail.com>
2023-10-01 15:15:43 -0600

    Verify that `Task` state throws `States.Timeout` error on time out

--- __tests__/stateActions/TaskStateAction.test.ts
+++ __tests__/stateActions/TaskStateAction.test.ts
@@ -1,6 +1,8 @@
 import type { TaskState } from '../../src/typings/TaskState';
 import { LambdaClient } from '../../src/aws/LambdaClient';
 import { TaskStateAction } from '../../src/stateMachine/stateActions/TaskStateAction';
+import { StatesTimeoutError } from '../../src/error/predefined/StatesTimeoutError';
+import { sleep } from '../../src/util';
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -69,5 +71,49 @@ describe('Task State', () => {
     expect(localHandlerFn).toHaveBeenCalledWith(input);
     expect(mockInvokeFunction).not.toHaveBeenCalled();
     expect(stateResult).toBe(8);
+  });
+
+  test('should throw `States.Timeout` error if action runs longer than the value specified in `TimeoutSeconds` field', async () => {
+    const definition: TaskState = {
+      Type: 'Task',
+      Resource: 'mock-arn',
+      TimeoutSeconds: 1,
+      End: true,
+    };
+    const stateName = 'TaskState';
+    const input = { num1: 5, num2: 3 };
+    const context = {};
+
+    const localHandlerFn = async () => {
+      await sleep(1100);
+      return 1;
+    };
+    const options = { overrideFn: localHandlerFn, awsConfig: undefined };
+
+    const taskStateAction = new TaskStateAction(definition, stateName);
+
+    await expect(() => taskStateAction.execute(input, context, options)).rejects.toThrow(StatesTimeoutError);
+  });
+
+  test('should throw `States.Timeout` error if action runs longer than the value specified in `TimeoutSecondsPath` field', async () => {
+    const definition: TaskState = {
+      Type: 'Task',
+      Resource: 'mock-arn',
+      TimeoutSecondsPath: '$.taskTimeout',
+      End: true,
+    };
+    const stateName = 'TaskState';
+    const input = { num1: 5, num2: 3, taskTimeout: 1 };
+    const context = {};
+
+    const localHandlerFn = async () => {
+      await sleep(1100);
+      return 1;
+    };
+    const options = { overrideFn: localHandlerFn, awsConfig: undefined };
+
+    const taskStateAction = new TaskStateAction(definition, stateName);
+
+    await expect(() => taskStateAction.execute(input, context, options)).rejects.toThrow(StatesTimeoutError);
   });
 });
