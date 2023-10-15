891e4f719ff239d9c1f0ec7e0f2d5514ce3b89df
Luis De Anda <ldeandac16@gmail.com>
2023-10-15 14:54:41 -0600

    Add support for `ErrorPath` and `CausePath` fields

--- src/stateMachine/stateActions/FailStateAction.ts
+++ src/stateMachine/stateActions/FailStateAction.ts
@@ -4,6 +4,8 @@ import type { ActionResult, FailStateActionOptions } from '../../typings/StateAc
 import type { Context } from '../../typings/Context';
 import { BaseStateAction } from './BaseStateAction';
 import { FailStateError } from '../../error/FailStateError';
+import { jsonPathQuery } from '../jsonPath/JsonPath';
+import { StringConstraint } from '../jsonPath/constraints/StringConstraint';
 
 class FailStateAction extends BaseStateAction<FailState> {
   constructor(stateDefinition: FailState, stateName: string) {
@@ -18,7 +20,23 @@ class FailStateAction extends BaseStateAction<FailState> {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     options?: FailStateActionOptions
   ): Promise<ActionResult> {
-    throw new FailStateError(this.stateDefinition.Error, this.stateDefinition.Cause);
+    const state = this.stateDefinition;
+
+    let error = state.Error;
+    let cause = state.Cause;
+
+    if (state.ErrorPath) {
+      error = jsonPathQuery<string>(state.ErrorPath, input, context, {
+        constraints: [StringConstraint],
+      });
+    }
+    if (state.CausePath) {
+      cause = jsonPathQuery<string>(state.CausePath, input, context, {
+        constraints: [StringConstraint],
+      });
+    }
+
+    throw new FailStateError(error, cause);
   }
 }
 
